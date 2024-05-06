54bd4fc32395cce902e7b5008fc9411a4972a913
eftdyguh <gs1icarodsi@hotmail.com>
2024-05-06 11:55:56 +0700

    feat project

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const name = "john";
-const upperName = name.toUpperCase();
-console.log(upperName); // JOHN
+const bubbleSortOptimized = (arr) => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+};
