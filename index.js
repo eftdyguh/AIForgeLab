c8fb9746ba1df537892f0513e75202fe21ca800b
eftdyguh <gs1icarodsi@hotmail.com>
2024-05-12 09:13:42 +0700

    start

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("world");
+function isValidParentheses(s) {
+  const stack = [];
+  const map = { "(": ")", "[": "]", "{": "}" };
+  for (const char of s) {
+    if (char in map) stack.push(char);
+    else {
+      const top = stack.pop();
+      if (map[top] !== char) return false;
+    }
+  }
+  return stack.length === 0;
+}
